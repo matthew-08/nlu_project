a7bbdc558e182d8e0d7bb6b39ca4a4c99f9c39e3
Matthew <matthewjcrosby1@gmail.com>
2023-06-15 13:19:05 +0700

    [FEAT]: Fetch categories handler inside of use nav items hook

--- client/src/hooks/useNavItems.ts
+++ client/src/hooks/useNavItems.ts
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { list } from '@chakra-ui/react';
 import APP_CAPABILITIES from '../utils/capabilites';
 import { DropdownItem } from '../types';
 import capitalize from '../utils/capitalize';
+import appFetch from '../utils/appFetch';
 
 type ListItem = {
   dropdown: boolean;
@@ -40,6 +42,26 @@ const useNavItems = () => {
       name: 'Contact',
     },
   ]);
+  const fetchCategories = async () => {
+    const cats = await appFetch({
+      method: 'GET',
+      path: '/api/categories',
+    }).then((r) => r.json());
+    const updatedState = listItems.map((i) => {
+      if (i.name === 'Flavors') {
+        return {
+          ...i,
+          dropdownItems: cats,
+        };
+      }
+      return i;
+    });
+    setListItems([...updatedState]);
+  };
+  useEffect(() => {
+    fetchCategories();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return { listItems };
 };
 
